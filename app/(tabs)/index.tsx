d97cb65d5091a860daea60ab955dedfb540c860a
Kuah Zhao Yang <zykuah2006@gmail.com>
2026-02-01 19:03:30 -0500

    Formatting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -199,7 +199,7 @@ const Index = () => {
                             )}
                         </>
                     ) : (
-                        <Text style={styles.text}>No hikes recorded yet.</Text>
+                        <Text style={[styles.text, { textAlign: 'center' }]}>No hikes yet.</Text>
                     )}
                 </View>
             </ScrollView >
